933d2981e20eddc4a1786d6b1eaaf5d90dfbbae4
joeyaly <joey.aly8@gmail.com>
2020-10-17 20:01:33 -0400

    Created routes

--- src/componets/Navbar/Navbar.js
+++ src/componets/Navbar/Navbar.js
@@ -1,10 +1,49 @@
-import React from 'react'
-import {Nav} from './Navbar.elements';
+import React, {useState} from 'react'
+import{FaBars, FaTimes} from  'react-icons/fa';
+import { IconContext} from 'react-icons/lib'
+import {
+    Nav, 
+    NavbarContainer, 
+    NavLogo, 
+    MobileIcon,
+    NavMenu,
+    NavItem,
+    NavLinks
+    } from './Navbar.elements';
+
 
 const Navbar = () => {
+    const [click, setClick] = useState(false)
+
+    const handleClick = () => setClick(!click)
     return (
         <>
-            <Nav>Test</Nav>
+        <IconContext.Provider value={{ color: '#fff'}}>
+            <Nav>
+                <NavbarContainer>
+                    <NavLogo to="/">
+                        DAS
+
+                    </NavLogo>
+                    <MobileIcon onClick={handleClick}>
+                        {click ? <FaTimes /> : <FaBars />}
+                    </MobileIcon>
+                    <NavMenu onCLick={handleClick} click={click}>
+                        <NavItem>
+                            <NavLinks to='/'>Home</NavLinks>
+                        </NavItem>                   
+                        <NavItem>
+                            <NavLinks to='/services'>Services</NavLinks>
+                        </NavItem>                    
+                        <NavItem>
+                            <NavLinks to='/products'>Products</NavLinks>
+                        </NavItem>
+                   
+                    </NavMenu>
+                </NavbarContainer> 
+ 
+            </Nav>
+        </IconContext.Provider>
         </>
     );
 };
